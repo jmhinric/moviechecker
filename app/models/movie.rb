84d5dac5617b6b9644ed88da40030e8df2a31b16
John Hinrichs <jmhinric@gmail.com>
2014-04-21 14:46:51 -0400

    change api to rotten tomatoes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,12 @@ class Movie < ActiveRecord::Base
     search_title = title.gsub(' ', '+')
 
     omdb_json = HTTParty.get "http://www.omdbapi.com/?t=#{search_title}"
-    binding.pry
     omdb = JSON(omdb_json)
-    return omdb["Poster"]
+    # return omdb["Poster"]
+
+    rot_tom_json = HTTParty.get "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{ROTTEN_TOM}&q=#{search_title}&page_limit=1"
+    rot_tom = JSON(rot_tom_json)
+    # binding.pry
+    return rot_tom["movies"][0]["posters"]["original"]
   end
 end
