7c9ece9ed52aaee3bef14e33a28e973cdde8e223
John Hinrichs <jmhinric@gmail.com>
2014-04-21 13:30:07 -0400

    change img tag

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ActiveRecord::Base
     search_title = title.gsub(' ', '+')
 
     omdb_json = HTTParty.get "http://www.omdbapi.com/?t=#{search_title}"
+    binding.pry
     omdb = JSON(omdb_json)
     return omdb["Poster"]
   end
